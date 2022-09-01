410d898cb80c6d97dbc54413e47a776ba2f96fae
Adam Topczewski <adamtopczewski@Adams-MacBook-Air.local>
2022-09-01 12:49:42 +0200

    changed docker compose for mac local dev; linted

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,12 @@ import { DatabaseModule } from './database/database.module';
     PostsModule, 
     ConfigModule.forRoot({    
       validationSchema: Joi.object({
-      POSTGRES_HOST: Joi.string().required(),
-      POSTGRES_PORT: Joi.number().required(),
-      POSTGRES_USER: Joi.string().required(),
-      POSTGRES_PASSWORD: Joi.string().required(),
-      POSTGRES_DB: Joi.string().required(),
-      PORT: Joi.number()
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number()
     })}
   ), 
   DatabaseModule
